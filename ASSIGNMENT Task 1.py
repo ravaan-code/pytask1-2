fe8cca15c05f2362e280d9b7adf895448c39f4a1
ravaan-code <biswajitmondal198823@gmail.com>
2025-06-21 09:08:31 +0530

    Add files via upload

---
+++ ASSIGNMENT Task 1.py
@@ -0,0 +1,11 @@
+a = int(input('first number: '))
+b = int(input('second number: '))
+Addition = (a+b)
+Subtraction = (a-b)
+Multiplication = (a*b)
+Division = (a/b)
+
+print('Addition', Addition)
+print('Subtraction', Subtraction)
+print('Multiplication', Multiplication)
+print('Division', Division)
